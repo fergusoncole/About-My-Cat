aa716ca6fb8f3d97ce7c9c65d308c76c2add55e6
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-01 11:24:18 -0500

    About my Cat

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string A = "hello";
+string catname = "Numbers";
+int catage = 10;
+Char C = 'D';
+bool D = false;
+double E = 2.2;
+decimal value = .22M;
+
+
+
+Console.WriteLine("Hello let me tell you about my cat!");
+Console.WriteLine($"My cats name is {catname}, he is {catage} old");
+
